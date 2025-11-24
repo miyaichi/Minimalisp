ebb7fda75083b8eb74ba02172ab65645147ccbee
Yoshihiko Miyaichi <miyaichi@gmail.com>
2025-11-24 11:16:06 +0900

    Rewrite queens example with Lisp helpers

--- queens.lisp
+++ queens.lisp
@@ -1,30 +1,55 @@
-(define (abs x)
-  (if (< x 0) (- 0 x) x))
+(define (not x)
+  (if x '() t))
 
-(define (safe? col queens)
-  (let loop ((qs queens) (row 1))
-    (if (null? qs)
-        #t
-        (let ((c (car qs)))
-          (if (or (= c col)
-                  (= (- c row) (- col (length queens)))
-                  (= (+ c row) (+ col (length queens))))
-              #f
-              (loop (cdr qs) (+ row 1)))))))
+(define (null? x)
+  (if x '() t))
 
-(define (extend queens n size)
-  (if (> n size)
-      (begin
-        (print queens)
-        (list queens))
-      (let ((solutions '()))
-        (let try ((col 1))
-          (if (> col size)
-              solutions
-              (let ((new (cons col queens)))
-                (if (safe? col queens)
-                    (set! solutions (append (extend new (+ n 1) size) solutions)))
-                (try (+ col 1))))))))
+(define (append xs ys)
+  (if xs
+      (cons (car xs) (append (cdr xs) ys))
+      ys))
+
+(define (reverse-iter src acc)
+  (if src
+      (reverse-iter (cdr src) (cons (car src) acc))
+      acc))
+
+(define (reverse xs)
+  (reverse-iter xs '()))
+
+(define (emit-solution solution)
+  (print solution)
+  (list solution))
+
+(define (conflict? column value offset)
+  (if (= column value)
+      t
+      (if (= column (+ value offset))
+          t
+          (if (= column (- value offset))
+              t
+              '()))))
+
+(define (safe? column queens offset)
+  (if (null? queens)
+      t
+      (if (conflict? column (car queens) offset)
+          '()
+          (safe? column (cdr queens) (+ offset 1)))))
+
+(define (extend-columns queens row size col accum)
+  (if (> col size)
+      accum
+      (if (safe? col queens 1)
+          (extend-columns queens row size (+ col 1)
+                           (append (extend (cons col queens) (+ row 1) size)
+                                   accum))
+          (extend-columns queens row size (+ col 1) accum))))
+
+(define (extend queens row size)
+  (if (> row size)
+      (emit-solution (reverse queens))
+      (extend-columns queens row size 1 '())))
 
 (define (n-queens size)
   (extend '() 1 size))
